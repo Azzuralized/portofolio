91492335fdf509300798bffa8ce8c78ce05f3cce
codebylanre <140360554+codebylanre@users.noreply.github.com>
2024-08-25 12:59:37 +0100

    Little updates to texts

--- src/pages/Main/PageThree.jsx
+++ src/pages/Main/PageThree.jsx
@@ -8,7 +8,7 @@ const PageThree = () => {
             <div className='three'>
 
 
-                <div className='focal'>LET’S GET <br />SOCIAL!<br />
+                <div className='focal'>Let's Get <br />Social!<br />
                     <div className='text'>CONTACT</div>
                     <div className='footer'>Lanre
                         <div className='desc'>A gentle rebellious studio who believes<br /> content is fire but social media is gasoline</div>
@@ -18,7 +18,7 @@ const PageThree = () => {
                 <div>
                     <div className='contact'>
                         <div className='mail'>Mail
-                            <div>Codebylanre@gmail.com</div>
+                            <div>withlanre@gmail.com</div>
                         </div>
                     </div>
                 </div>
